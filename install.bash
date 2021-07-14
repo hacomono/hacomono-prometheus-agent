0742e53e8a11fae9968945f02c8f787cf1735bb8
Yusuke Yajima <yusuke.yajima@hacomono.co.jp>
2021-07-14 13:49:04 +0900

    Add install bash

---
+++ install.bash
@@ -0,0 +1,42 @@
+#! /bin/bash
+set -ue
+set -o pipefail
+
+yum install -y https://github.com/martin-helmich/prometheus-nginxlog-exporter/releases/download/v1.9.0/prometheus-nginxlog-exporter_1.9.0_linux_amd64.deb
+mv /etc/prometheus-nginxlog-exporter.hcl /etc/prometheus-nginxlog-exporter.hcl.orig
+cat > /etc/prometheus-nginxlog-exporter.hcl <<'_EOD_'
+listen {
+  port = 4040
+}
+
+namespace "nginx" {
+  source = {
+    files = [
+      "/var/log/nginx/access.log"
+    ]
+  }
+
+  format = "$remote_addr - $remote_user [$time_local] \"$request\" $status $body_bytes_sent \"$http_referer\" \"$http_user_agent\""
+
+  relabel "request_uri" {
+    from = "request"
+    split = 2
+    separator = " "
+    match "^/([^?]*).*" {
+      replacement = "/$1"
+    }
+  }
+
+  relabel "host" {
+    from = "server_name"
+  }
+
+  labels {
+    app = "default"
+  }
+}
+_EOD_
+
+systemctl enable prometheus-nginxlog-exporter
+systemctl start prometheus-nginxlog-exporter
+systemctl status prometheus-nginxlog-exporter
