71fc352ef2150770519c93e70bdfea0cd205f8c6
Yusuke Yajima <yusuke.yajima@hacomono.co.jp>
2021-08-17 12:39:25 +0900

    Fix for logratate & amzn1

--- install.bash
+++ install.bash
@@ -37,6 +37,26 @@ namespace "nginx" {
 }
 _EOD_
 
-systemctl enable prometheus-nginxlog-exporter
-systemctl start prometheus-nginxlog-exporter
-systemctl status prometheus-nginxlog-exporter
+cat > /etc/logrotate.d/nginx.patch <<'_EOD_'
+--- nginx.orig  2021-08-17 12:25:44.841912093 +0900
++++ nginx       2021-08-17 12:26:41.358451393 +0900
+@@ -8,5 +8,6 @@
+     sharedscripts
+     postrotate
+         /etc/init.d/nginx reload
++        service prometheus-nginxlog-exporter restart
+     endscript
+ }
+_EOD_
+(
+  cd /etc/logrotate.d
+  cp nginx nginx.orig."$(date '+%Y%m%d')"
+  git apply nginx.patch
+)
+
+
+# No use systemctl for Amazon Linux 1
+service prometheus-nginxlog-exporter restart
+#systemctl enable prometheus-nginxlog-exporter
+#systemctl start prometheus-nginxlog-exporter
+#systemctl status prometheus-nginxlog-exporter
